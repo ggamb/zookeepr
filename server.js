ccb9d1e5fe6bfa1dff544df04caa47cfa0ef71d7
ggamb <glenngambardella372@gmail.com>
2021-10-11 14:17:29 -0400

    working find by id

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const { animals } = require('./data/animals.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
   let filteredResults = animalsArray;
@@ -35,6 +40,12 @@ function findById(id, animalsArray) {
   const result = animalsArray.filter(animal => animal.id === id)[0];
   return result;
 }
+
+app.post('/api/animals', (req, res) => {
+  // req.body is where our incoming content will be
+  console.log(req.body);
+  res.json(req.body);
+});
 
 app.get('/api/animals', (req, res) => {
   let results = animals;
